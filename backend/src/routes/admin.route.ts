46544bfd77a53942e4cab2094e293eaa2b3c6409
lucifer0x17 <ayush.aa347@gmail.com>
2023-05-26 19:58:06 +0530

    init: structure

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -1,7 +1,4 @@
 import express from "express"
-import validate from "../middlewares/validateResources"
-import { addAwardRequestSchema } from "../schemas/prize.schema"
-import { addAwardController } from "../controllers/admin.controller"
 
 const router = express.Router()
 
@@ -11,10 +8,6 @@ router.get('/', (_req, res) => {
     // })
     res.status(200).json("This is admin route")
 })
-
-router.post('/add-award', validate(addAwardRequestSchema), addAwardController)
-
-router.get('/profile')
 
 
 export default router
