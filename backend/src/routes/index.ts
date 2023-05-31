e21c752549a450beea59a65c2262961ab9699ec9
Yash Garg <yashygkf@gmail.com>
2023-05-31 22:30:59 +0530

    add: routes added

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,24 @@
-import express from "express"
-import adminRoutes from "./admin.route"
+import express from "express";
+import adminRoutes from "./admin.route";
+import mintRoutes from "./mint.route";
+import fetchAccountRoutes from "./fetchAccount.route";
+import fetchTokenIdRoutes from "./fetchTokenId.route";
+import getAllNFTRoutes from "./getAllNFT.route";
+import safeBatchTransferFromTxnRoutes from "./safeBatchTransferFromTxn.route";
+import transferAssetsTxnDataRoutes from "./transferAssetsTxnData.route";
+// import uploadImageRoutes from "./uploadImage.route";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (_, res) => res.status(200).send("Healthy"));
+router.get("/", (_, res) => res.status(200).send("Healthy"));
 
+router.use("/admin", adminRoutes);
+router.get("/mint", mintRoutes);
+router.get("/fetchTokenId", fetchTokenIdRoutes);
+router.get("/fetchAccount", fetchAccountRoutes);
+router.get("/getAllNFT", getAllNFTRoutes);
+router.get("/safeBatchTransferFromTxn", safeBatchTransferFromTxnRoutes);
+router.get("/transferAssetsTxnData", transferAssetsTxnDataRoutes);
+// router.get("/uploadImage", uploadImageRoutes);
 
-router.use('/admin', adminRoutes)
-
-
-export default router
+export default router;
