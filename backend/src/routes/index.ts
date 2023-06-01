1c87ba2ea788e98be49b23aa3f069caeb9ae3cc2
lucifer0x17 <ayush.aa347@gmail.com>
2023-06-01 05:58:40 +0530

    fix: routes added to index

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,24 +1,15 @@
 import express from "express";
-import adminRoutes from "./admin.route";
-import mintRoutes from "./mint.route";
-import fetchAccountRoutes from "./fetchAccount.route";
-import fetchTokenIdRoutes from "./fetchTokenId.route";
-import getAllNFTRoutes from "./getAllNFT.route";
-import safeBatchTransferFromTxnRoutes from "./safeBatchTransferFromTxn.route";
-import transferAssetsTxnDataRoutes from "./transferAssetsTxnData.route";
+import accountRoutes from "./account.route"
+import nftRoutes from "./nft.route"
+
 // import uploadImageRoutes from "./uploadImage.route";
 
 const router = express.Router();
 
 router.get("/", (_, res) => res.status(200).send("Healthy"));
 
-router.use("/admin", adminRoutes);
-router.get("/mint", mintRoutes);
-router.get("/fetchTokenId", fetchTokenIdRoutes);
-router.get("/fetchAccount", fetchAccountRoutes);
-router.get("/getAllNFT", getAllNFTRoutes);
-router.get("/safeBatchTransferFromTxn", safeBatchTransferFromTxnRoutes);
-router.get("/transferAssetsTxnData", transferAssetsTxnDataRoutes);
-// router.get("/uploadImage", uploadImageRoutes);
+router.use("/account", accountRoutes);
+router.use("/nft", nftRoutes);
+
 
 export default router;
