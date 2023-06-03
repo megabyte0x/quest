70249a0d04bfbd860f79a13f57bdcb82f63916ed
Yash Garg <yashygkf@gmail.com>
2023-06-04 01:46:36 +0530

    feat: integration for fetchAccoutn route

--- frontend/components/fetchAccount.tsx
+++ frontend/components/fetchAccount.tsx
@@ -5,30 +5,20 @@ import React, { useState } from "react";
 export default function FetchAccount() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [txnLink, setTxnLink] = useState("");
+  const [tkba, setTkba] = useState("");
   const [tokenId, setTokenId] = useState("0");
 
   const handleTKBA = async () => {
     setLoading(true);
     try {
-      await fetch("/api/fetchAccount", {
+      const res = await fetch("/api/fetchAccount", {
         method: "POST",
         body: JSON.stringify({
           tokenId: tokenId,
         }),
-      }).then((res) => {
-        console.log(res);
-        if (res.status === 200) {
-          console.log(res.json());
-          res.json().then((data) => {
-            console.log(data);
-            setTxnLink(data.tkbaAddress);
-          });
-        } else {
-          setError("Error");
-          setLoading(false);
-        }
       });
+      const tkbAddress = await res.json();
+      setTkba(tkbAddress);
     } catch (err: any) {
       console.log(err);
       setError(err.message);
@@ -39,6 +29,7 @@ export default function FetchAccount() {
   return (
     <div>
       <button onClick={handleTKBA}>Get TBA</button>
+      {tkba ? <p>{tkba}</p> : <p>TKBA not found</p>}
     </div>
   );
 }
--- frontend/pages/api/fetchAccount.ts
+++ frontend/pages/api/fetchAccount.ts
@@ -7,23 +7,20 @@ export default async function handler(req: any, res: any) {
   }
 
   try {
-    console.log(JSON.stringify({ tokenId: tokenId }));
-    const response = await fetch("http://localhost:5001/api/v1/fetchAccount", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ tokenId: tokenId }),
-    });
-    console.log(response);
-    console.log("Response in JSION:", await response.json());
-    await response.json().then((data) => {
-      console.log("DATA:", data);
-      console.log(data.tkbAddress);
-      res.status(200).json(data.tkbAddress);
-    });
-    // console.log("DATA:", data);
-    // console.log(data.tkbAddress);
+    const response = await fetch(
+      "http://localhost:5001/api/v1/account/fetchAccount",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ tokenId: tokenId }),
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data.tkbAddress);
   } catch (error) {
     res.status(500).json({ message: "Something went wrong while miniting" });
     console.error("Error in /fetchAccount Frontend API:", error);
